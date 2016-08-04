a9639a808118871215badb40ec51e159f0010242
Stéphane Schoorens <stephane.schoorens@ubicast.eu>
2016-08-04 15:22:31 +0200

    add dist dir

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/* globals require */
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var minify = require("gulp-minify");
+var cleanCSS = require("gulp-clean-css");
+var rename = require("gulp-rename");
+
+gulp.task("build", function() {
+  gulp.src([
+    "vendors/js/jquery-latest.min.js",
+    "vendors/js/utils.js",
+    "js/ubi-web-lib-base.js"
+  ])
+  .pipe(concat("ubi-web-lib.js"))
+  .pipe(minify({
+    compress: {
+      hoist_vars: true
+    }
+  }))
+  .pipe(gulp.dest("./dist/js"));
+  return gulp.src([
+    "vendors/css/animate.min.css",
+    "vendors/css/normalize.css",
+    "css/animation.css",
+    "css/base.css",
+    "css/input.css",
+    "css/print.css",
+    "css/structure.css",
+    "css/table.css"
+  ])
+  .pipe(concat("ubi-web-lib.min.css"))
+  .pipe(cleanCSS({ compatibility: "ie8" }))
+  .pipe(gulp.dest("./dist/css"));
+});
