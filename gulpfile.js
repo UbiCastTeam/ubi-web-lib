09f6916899d51817eff13226e8669be5b9e44946
Stéphane Diemer <stephane.diemer@ubicast.eu>
2021-01-25 11:03:20 +0100

    Drop utils | refs #33317

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
 /* globals require */
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var minify = require("gulp-minify");
-var cleanCSS = require("gulp-clean-css");
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task("build", function() {
+gulp.task('build', function() {
   gulp.src([
-    "vendors/js/jsu.js",
-    "vendors/js/utils.js",
-    "vendors/js/odm/odm.js",
-    "vendors/js/jquery.min.js",
-    "js/ubi-web-lib-base.js"
+    'vendors/js/jsu.js',
+    'vendors/js/odm/odm.js',
+    'vendors/js/jquery.min.js',
+    'js/ubi-web-lib-base.js'
   ])
-  .pipe(concat("ubi-web-lib.js"))
+  .pipe(concat('ubi-web-lib.js'))
   .pipe(minify({
     compress: {
-      hoist_vars: true,
-   //   screw_ie8: true
+      // screw_ie8: true,
+      hoist_vars: true
     }
   }))
-  .pipe(gulp.dest("./dist"));
+  .pipe(gulp.dest('./dist'));
   return gulp.src([
-    "vendors/css/normalize.css",
-    "vendors/css/animate.min.css",
-    "vendors/js/odm/odm.css",
-    "css/animation.css",
-    "css/base.css",
-    "css/input.css",
-    "css/structure.css",
-    "css/table.css",
-    "css/responsive.css",
-    "css/tools.css",
-    "css/colors.css"
+    'vendors/css/normalize.css',
+    'vendors/css/animate.min.css',
+    'vendors/js/odm/odm.css',
+    'css/animation.css',
+    'css/base.css',
+    'css/input.css',
+    'css/structure.css',
+    'css/table.css',
+    'css/responsive.css',
+    'css/tools.css',
+    'css/colors.css'
   ])
-  .pipe(concat("ubi-web-lib.min.css"))
-  .pipe(cleanCSS({ compatibility: "ie8" }))
-  .pipe(gulp.dest("./dist"));
+  .pipe(concat('ubi-web-lib.min.css'))
+  .pipe(cleanCSS({ compatibility: 'ie8' }))
+  .pipe(gulp.dest('./dist'));
 });
